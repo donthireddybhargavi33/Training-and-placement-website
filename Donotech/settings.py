9431d7862331b602842229555fb727bfe7742726
Bhargavi <rbhargavi846@gmail.com>
2025-08-19 09:45:50 +0530

    settings

--- Donotech/settings.py
+++ Donotech/settings.py
@@ -165,8 +165,6 @@ JAZZMIN_SETTINGS = {
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "Donotech",
 
-    # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "books/img/logo.png",
 
     # Logo to use for login form logo (defaults to site_logo)
     "login_logo": None,
